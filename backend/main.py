dbfa5a0e231a38178797847b5a330c1116fa525c
vvduth <ducthai060501@gmail.com>
2025-12-19 19:02:06 +0200

    get the m,achine imahe and get the music model

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,9 @@ app = modal.App("dukem-music")
 
 image = (
     modal.Image.debian_slim()
+    .apt_install("git")
+    # clone the music genration mopdel repo
+    .run_commands(["git clone https://github.com/ace-step/ACE-Step.git /tmp/ACE-Step",
+                   "cd /tmp/ACE-Step && pip install ."])
+    .env({"HF_HOME": })
 )

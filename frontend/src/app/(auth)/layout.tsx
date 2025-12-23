ac0bddf9c0632c7b97a4592f3b7b44bc055ece68
Dukem <ducthai060501@gmail.com>
2025-12-23 02:19:17 +0200

    feat: update package.json to include Inngest CLI and dependencies
    refactor: simplify AuthLayout component to return children directly
    
    fix: update PrismaClient import path to use @prisma/client

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,14 +1,6 @@
-import { Providers } from "~/components/providers";
-import "~/styles/globals.css";
 export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-        <body className="flex min-h-svh flex-col">
-            <Providers>{children}</Providers>
-        </body>
-    </html>
-  );
+  return children;
 }   
   
